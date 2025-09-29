236e52b40dc4e965e51e6538cf21cdc8c84ea65e
Tanghai Ly <tanghai.ly@wingbank.com.kh>
2025-09-29 13:35:04 +0700

    set callback webhook

---
+++ src/main/java/com/tanghai/announcement/controller/TelegramWebhookController.java
@@ -0,0 +1,28 @@
+package com.tanghai.announcement.controller;
+
+import com.tanghai.announcement.component.TelegramPollingBot;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.meta.api.methods.updates.DeleteWebhook;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@RestController
+public class TelegramWebhookController {
+
+    private final TelegramPollingBot bot;
+
+    public TelegramWebhookController(TelegramPollingBot bot) {
+        this.bot = bot;
+    }
+
+    @PostMapping("/telegram/delete-webhook")
+    public String deleteWebhook() {
+        try {
+            bot.execute(new DeleteWebhook());
+            return "Webhook deleted successfully!";
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+            return "Failed to delete webhook: " + e.getMessage();
+        }
+    }
+}
