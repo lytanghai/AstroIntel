f50eee7d718b732a56549974d1099bd02b25d4db
unknown <lytanghaii@gmail.com>
2025-10-01 18:35:39 +0700

    add web socket bybit

--- src/main/java/com/tanghai/announcement/AnnouncementApplication.java
+++ src/main/java/com/tanghai/announcement/AnnouncementApplication.java
@@ -1,10 +1,7 @@
 package com.tanghai.announcement;
 
-import com.tanghai.announcement.component.BybitXauUsdClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -15,8 +12,8 @@ public class AnnouncementApplication {
 		SpringApplication.run(AnnouncementApplication.class, args);
 	}
 
-	@EventListener(ApplicationReadyEvent.class)
-	public void startWebSocket(BybitXauUsdClient client) {
-		client.connect();
-	}
+//	@EventListener(ApplicationReadyEvent.class)
+//	public void startWebSocket(BybitXauUsdClient client) {
+//		client.connect();
+//	}
 }
--- src/main/java/com/tanghai/announcement/component/BybitXauUsdClient.java
+++ src/main/java/com/tanghai/announcement/component/BybitXauUsdClient.java
@@ -1,44 +1,44 @@
-package com.tanghai.announcement.component;
-
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.handshake.ServerHandshake;
-import org.springframework.stereotype.Component;
-
-import java.net.URI;
-
-@Component
-public class BybitXauUsdClient extends WebSocketClient {
-
-    public BybitXauUsdClient() {
-        super(URI.create("wss://stream.bybit.com/realtime_public"));
-    }
-
-    @Override
-    public void onOpen(ServerHandshake handshake) {
-        System.out.println("Connected to Bybit");
-        send("{\"op\": \"subscribe\", \"args\": [\"tickers.XAUUSD\"]}");
-    }
-
-    @Override
-    public void onMessage(String message) {
-        if (message.contains("\"topic\":\"tickers.XAUUSD\"")) {
-            try {
-                // Extract last_price (simplified parsing)
-                String price = message.split("\"last_price\":\"")[1].split("\"")[0];
-                System.out.println("XAU/USD Price: " + price);
-            } catch (Exception e) {
-                System.out.println("Parsing update: " + message);
-            }
-        }
-    }
-
-    @Override
-    public void onClose(int code, String reason, boolean remote) {
-        System.out.println("Connection closed: " + reason);
-    }
-
-    @Override
-    public void onError(Exception ex) {
-        System.err.println("Error: " + ex.getMessage());
-    }
-}   
+//package com.tanghai.announcement.component;
+//
+//import org.java_websocket.client.WebSocketClient;
+//import org.java_websocket.handshake.ServerHandshake;
+//import org.springframework.stereotype.Component;
+//
+//import java.net.URI;
+//
+//@Component
+//public class BybitXauUsdClient extends WebSocketClient {
+//
+//    public BybitXauUsdClient() {
+//        super(URI.create("wss://stream.bybit.com/realtime_public"));
+//    }
+//
+//    @Override
+//    public void onOpen(ServerHandshake handshake) {
+//        System.out.println("Connected to Bybit");
+//        send("{\"op\": \"subscribe\", \"args\": [\"tickers.XAUUSD\"]}");
+//    }
+//
+//    @Override
+//    public void onMessage(String message) {
+//        if (message.contains("\"topic\":\"tickers.XAUUSD\"")) {
+//            try {
+//                // Extract last_price (simplified parsing)
+//                String price = message.split("\"last_price\":\"")[1].split("\"")[0];
+//                System.out.println("XAU/USD Price: " + price);
+//            } catch (Exception e) {
+//                System.out.println("Parsing update: " + message);
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public void onClose(int code, String reason, boolean remote) {
+//        System.out.println("Connection closed: " + reason);
+//    }
+//
+//    @Override
+//    public void onError(Exception ex) {
+//        System.err.println("Error: " + ex.getMessage());
+//    }
+//}
